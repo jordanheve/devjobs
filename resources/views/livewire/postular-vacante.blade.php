b9c3edab0b9130efab9a1ae87d32d2aa791ea26a
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2024-03-28 18:57:40 -0600

    Refactor postular-vacante.blade.php layout

--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,20 +1,17 @@
 <div class="bg-slate-700 p-5 mt-8 flex-col justify-center items-center flex">
-    @if (!session()->has('mensaje'))
+    @if(session()->has('mensaje'))
+    <div class="border-dashed border-2 border-teal-500 p-5 text-teal-600 font-bold text-3xl">
+        <p>
+            {{ session('mensaje') }}
+        </p>
+    </div>
+
+    @else
+
     <h3 class="text-3xl font-semibold dark:text-zinc-300 mb-4">
         Postularme a esta vacante
     </h3>
-    @endif
-    <form wire:submit.prevent="postular" class="flex flex-col items-center text-center"
-        @if(session()->has('mensaje'))
-
-        <div class="border-dashed border-2 border-teal-500 p-5 text-teal-600 font-bold text-3xl">
-            <p>
-                {{ session('mensaje') }}
-            </p>
-        </div>
-
-
-        @else
+    <form wire:submit.prevent="postular" class="flex flex-col items-center text-center">
         <div class="mb-2">
             <x-input-label class="text-xl" for='cv' :value="__('Curriculum/Hoja de vida (PDF)')"/>
             <x-text-input class="block w-full mt-1" id="cv" type="file" name="cv" wire:model="cv" accept=".pdf"/>
@@ -27,6 +24,6 @@
         <x-primary-button class="w-fit mt-2">
             {{ __('Enviar CV') }}
         </x-primary-button>
-        @endif
     </form>
+    @endif
 </div>
